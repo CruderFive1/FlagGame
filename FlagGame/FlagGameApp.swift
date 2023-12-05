4063d08f8641b327841d695ac25dca79675aec04
Adrian <senften@outlook.com>
2023-12-05 16:56:07 +0100

    Initial Commit

---
+++ FlagGame/FlagGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlagGameApp.swift
+//  FlagGame
+//
+//  Created by Adrian Senften on 05.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct FlagGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
